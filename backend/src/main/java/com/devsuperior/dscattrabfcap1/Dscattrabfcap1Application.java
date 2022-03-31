b0774ee003a524a1c8801925555f20ccf9d9da6d
Frederico Rodarte <fredericorodarte12@gmail.com>
2022-03-31 18:20:49 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/dscattrabfcap1/Dscattrabfcap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscattrabfcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscattrabfcap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscattrabfcap1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscattrabfcap1/Dscattrabfcap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscattrabfcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscattrabfcap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
