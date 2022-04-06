8daff86e80f561ccbbb7a5c1e7a64822819aa441
Frederico Rodarte <fredericorodarte12@gmail.com>
2022-04-06 20:06:29 -0300

    Metodos gets criados e exceção tratada

---
+++ backend/src/main/java/com/devsuperior/dscattrabfcap1/services/ClientService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dscattrabfcap1.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscattrabfcap1.dto.ClientDTO;
+import com.devsuperior.dscattrabfcap1.entities.Client;
+import com.devsuperior.dscattrabfcap1.repositories.ClientRepository;
+import com.devsuperior.dscattrabfcap1.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional (readOnly = true) 
+	public List<ClientDTO> findAll(){
+		
+		List<Client> list = repository.findAll();
+		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		
+	}
+
+	@Transactional (readOnly = true) 
+	public ClientDTO findById(Long id) {		
+		
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		
+		return new ClientDTO(entity);
+	}
+}
